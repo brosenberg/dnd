d71192cb2048cee01a70756e887a37bc17125952
brosenberg <brosenberg@pistoncloud.com>
2013-10-09 09:54:44 -0700

    Old dice roller

---
+++ roll.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+for ( split(',', join('',@ARGV) ) ) {
+    if ( /^([0-9]+)\s*d\s*([0-9]+)\s*([-+\/*])?\s*([0-9]+)?\s*$/ ) {
+        my $mod = defined $3 && defined $4 ? "$3$4" : undef;
+        printf "%9s: ", sprintf "%sd%s%s", $1,$2,defined $mod?$mod:'';
+        &roll($1,$2,$mod);
+    }
+}
+
+sub roll {
+    my ($die, $sides, $mod) = @_;
+    my $total=0;
+    my @rolls;
+    if ( defined $die && defined $sides ) {
+        while ($die--) {
+            push @rolls, int(rand($sides))+1;
+            $total += $rolls[-1];
+        }
+        print "(", join('+',@rolls), ")";
+        if ( defined $mod ) {
+            print "$mod";
+            $total = eval "$total$mod";
+        }
+    }
+    print " = $total\n";
+}
