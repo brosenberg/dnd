b2ebae8e116c0c18b457b82eab3e3e91422c9691
brosenberg <brosenberg@pistoncloud.com>
2013-10-24 23:14:28 -0700

    make betterer

--- grep-trait.pl
+++ grep-trait.pl
@@ -9,10 +9,12 @@ while (<$FH>) {
     if (/$ARGV[0]/i) {
         my @l = split(',');
         map {s/^\s*"?(.+?)"?\s*$/$1/} @l;
-        my $s = sprintf("[1;33m%s[0m [1;34m(%s)[0m: %s\n",$l[0],$l[1],join(',',@l[5..$#l-2]));
-        $s =~ s/($ARGV[0])/[1;31m$1[0m/gi;
+        my $s = sprintf("[1;33m%s[0m [1;34m(%s)[0m: %s",$l[0],$l[1],join(', ',@l[5..$#l-2]));
+        if (length($ARGV[0])) {
+            $s =~ s/($ARGV[0])/[1;31m$1[0m/gi;
+        }
         $s =~ s/\s\s+/ /g;
-        print $s;
+        print "$s\n";
     }
 }
 close $FH;
