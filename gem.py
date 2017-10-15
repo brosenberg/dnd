65d33db46943e4ea9a1df07b4fdd3ba091b71d57
brosenberg <scdlbx@gmail.com>
2017-10-14 23:13:28 -0700

    Make gem generator better

--- gem.py
+++ gem.py
@@ -6,8 +6,10 @@ import random
 
 def main():
     gems = [0, 0, 0, 0, 0, 0]
+    values = (10, 50, 100, 500, 1000, 5000)
     try:
         count = int(sys.argv[1])
+        total = 0
         while count:
             roll = random.randint(1, 100)
             if roll == 100:
@@ -24,12 +26,11 @@ def main():
                 gems[0] += 1
 
             count = count - 1
-        print "%d 10gp gems" % (gems[0],)
-        print "%d 50gp gems" % (gems[1],)
-        print "%d 100gp gems" % (gems[2],)
-        print "%d 500gp gems" % (gems[3],)
-        print "%d 1000gp gems" % (gems[4],)
-        print "%d 5000gp gems" % (gems[5],)
+        for i in range(0, 6):
+            if gems[i]:
+                print "%d %s gp gems" % (gems[i], values[i])
+                total += gems[i] * values[i]
+        print "%d gp total" % (total,)
 
     except IndexError:
         print "Usage: %s [number of gems]" % (sys.argv[0],)
