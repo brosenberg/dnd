8a931a49679381f3653f20c6c009c8c04730159c
brosenberg <scdlbx@gmail.com>
2018-11-18 20:18:10 -0800

    Add gem variants

--- 1e-treasure/gem.py
+++ 1e-treasure/gem.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import sys
 import random
+import sys
 
-# TODO: Add Gem Variations
-
-gem_types = [
+VALUES = (10, 50, 100, 500, 1000, 5000)
+GEM_TYPES = [
     [
         'azurite',
         'banded agate',
@@ -75,35 +74,87 @@ gem_types = [
     ]
 ]
 
+def get_gem():
+    roll = random.randint(1, 100)
+    if roll == 100:
+        return 5
+    elif roll >= 91:
+        return 4
+    elif roll >= 71:
+        return 3
+    elif roll >= 51:
+        return 2
+    elif roll >= 26:
+        return 1
+    else:
+        return 0
+
+def gem_variant(base):
+    value = VALUES[base]
+    roll = random.randint(1, 6)
+    count = 0
+    if roll == 2:
+        value *= 2
+    elif roll == 3:
+        value = int(value*(1+(random.randint(1, 6)*0.1)))
+    elif roll == 4:
+        value = int(value*(1+(random.randint(1, 4)*0.1)))
+    elif roll == 5:
+        value = int(value/2)
+    while roll == 1:
+        if roll == 1:
+            if base < 6:
+                base += 1
+                value = VALUES[base]
+            else:
+                value *= 2
+                if value > 100000:
+                    return 100000
+        roll = random.randint(1, 6)
+    while roll == 6 and count < 5:
+        count += 1
+        if roll == 6:
+            if base > 0:
+                base -= 1
+                value = VALUES[base]
+            else:
+                if value == 10 or value == 1:
+                    value /= 2.0
+                elif value == 5:
+                    value = 1
+                else:
+                    return 0.1
+        roll = random.randint(1, 6)
+    return value
+
 def main():
     gems = [0, 0, 0, 0, 0, 0]
-    values = (10, 50, 100, 500, 1000, 5000)
-    try:
+    if len(sys.argv) > 1:
         count = int(sys.argv[1])
+        variant_count = 0
         total = 0
-        while count:
-            roll = random.randint(1, 100)
-            if roll == 100:
-                gems[5] += 1
-            elif roll >= 91:
-                gems[4] += 1
-            elif roll >= 71:
-                gems[3] += 1
-            elif roll >= 51:
-                gems[2] += 1
-            elif roll >= 26:
-                gems[1] += 1
-            else:
-                gems[0] += 1
-
-            count = count - 1
+        if count >= 10:
+            variant_count = int(count/10)
+            count = count - variant_count
+        for i in range(0, count):
+            gems[get_gem()] += 1
         for i in range(0, 6):
             if gems[i]:
-                print "%d %s gp %s" % (gems[i], values[i], random.choice(gem_types[i]))
-                total += gems[i] * values[i]
-        print "%d gp total" % (total,)
+                print "%d %s gp %s" % (gems[i], VALUES[i], random.choice(GEM_TYPES[i]))
+                total += gems[i] * VALUES[i]
+        if variant_count:
+            base = get_gem()
+            value = gem_variant(base)
+            total += value*variant_count
+            coin = "gp"
+            if value < 1:
+                value *= 10
+                coin = "sp"
+            value = int(value)
+            print "%d %s %s %s (variant)" % (variant_count, value, coin, random.choice(GEM_TYPES[base]))
 
-    except IndexError:
+        print "%d gp total" % (total,)
+    else:
         print "Usage: %s [number of gems]" % (sys.argv[0],)
         sys.exit(1)
 
