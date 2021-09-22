5a79d90f7da462a14cb2d18bd2dc27185ad2bf89
brosenberg <scdlbx@gmail.com>
2021-09-21 23:08:14 -0600

    Start to prepare for multiclassing

--- 2e/character.py
+++ 2e/character.py
@@ -93,6 +93,13 @@ def get_ability_priority(class_name):
     )
 
 
+def get_alignment_by_classes(classes):
+    alignments = set(CLASSES[classes[0]]["Alignments"])
+    for class_name in classes[1:]:
+        alignments = alignments.intersection(set(CLASSES[class_name]["Alignments"]))
+    return random.choice(alignments)
+
+
 def get_all_classes():
     return list(CLASSES.keys())
 
@@ -105,14 +112,25 @@ def get_caster_group(class_name):
     elif class_name == "Bard":
         caster = "Bard"
     else:
-        caster = get_class_group(class_name)
+        caster = get_class_groups([class_name])[0]
     if caster in SPELL_PROGRESSION.keys():
         return caster
     return None
 
 
+def get_caster_groups(classes):
+    return [get_caster_group(x) for x in classes]
+
+
 def get_class_group(class_name):
     return [x for x in CLASS_GROUPS if class_name in CLASS_GROUPS[x]["Classes"]][0]
+
+
+def get_class_groups(classes):
+    try:
+        return list(set([get_class_group(x) for x in classes]))
+    except:
+        breakpoint()
 
 
 def get_level_by_experience(class_name, experience):
@@ -122,7 +140,40 @@ def get_level_by_experience(class_name, experience):
     return 20
 
 
-def get_random_class(class_group=None, alignment=None):
+def get_levels_by_experience(classes, experience):
+    levels = []
+    exp_per_class = int(experience / len(classes))
+    for class_name in classes:
+        levels.append(get_level_by_experience(class_name, exp_per_class))
+    return levels
+
+
+def get_level_limits(race, classes, abilities):
+    limits = []
+    for class_name in classes:
+        limits.append(
+            RACES[race]["Limits"][class_name] + level_limit_bonus(class_name, abilities)
+        )
+    return limits
+
+
+def get_nwp_slots(class_groups, level, intelligence):
+    base_nwps = 0
+    nwp_rate = 99
+    for class_group in class_groups:
+        if CLASS_GROUPS[class_group]["Proficiencies"]["Nonweapon"][0] > base_nwps:
+            base_nwps = CLASS_GROUPS[class_group]["Proficiencies"]["Nonweapon"][0]
+        if CLASS_GROUPS[class_group]["Proficiencies"]["Nonweapon"][1] < nwp_rate:
+            nwp_rate = CLASS_GROUPS[class_group]["Proficiencies"]["Nonweapon"][1]
+    return (
+        base_nwps
+        + int(level / nwp_rate)
+        + intelligence_bonus_proficiencies(intelligence)
+    )
+
+
+# TODO: Make this return multiclasses sometimes
+def get_random_classes(class_group=None, alignment=None):
     classes = set(CLASSES.keys())
     if alignment:
         alignment_classes = set()
@@ -132,7 +183,7 @@ def get_random_class(class_group=None, alignment=None):
         classes = classes.intersection(alignment_classes)
     if class_group:
         classes = classes.intersection(set(CLASS_GROUPS[class_group]["Classes"]))
-    return random.choice(list(classes))
+    return [random.choice(list(classes))]
 
 
 def get_random_experience_by_level(class_name, level):
@@ -146,8 +197,20 @@ def get_random_experience_by_level(class_name, level):
         return xp + random.randint(0, xp / 10)
 
 
-def get_random_race_by_class(class_name):
-    return random.choice(CLASSES[class_name]["Races"])
+# TODO: Have this return an array instead
+def get_random_experience_by_levels(classes, level):
+    experience = 0
+    for class_name in classes:
+        experience += get_random_experience_by_level(class_name, level)
+    return experience
+
+
+def get_random_race_by_classes(classes):
+    races = set()
+    races = set(CLASSES[classes[0]]["Races"])
+    for class_name in classes[1:]:
+        races = races.intersection(set(CLASSES[class_name]["Races"]))
+    return random.choice(list(races))
 
 
 def get_spell_levels(class_name, level, wisdom):
@@ -191,7 +254,7 @@ def get_spell_subtype(class_name):
         return CLASS_SPELLS[class_name]["Subtype"]
     except KeyError:
         pass
-    caster = get_class_group(class_name)
+    caster = get_class_groups([class_name])[0]
     if caster == "Priest":
         return "Sphere"
     elif caster == "Wizard":
@@ -316,7 +379,7 @@ def wisdom_bonus_spells(wisdom):
 class Character(object):
     def __init__(
         self,
-        char_class=None,
+        classes=[],
         class_group=None,
         abilities=None,
         race=None,
@@ -326,80 +389,102 @@ class Character(object):
         expanded=False,
     ):
         self.expanded = expanded
-        self.char_class = char_class
-        if not self.char_class:
-            self.char_class = get_random_class(
+        self.classes = [classes]
+
+        # Determine a class
+        if not self.classes:
+            self.classes = get_random_classes(
                 class_group=class_group, alignment=alignment
             )
-        self.class_group = get_class_group(self.char_class)
+        self.class_groups = get_class_groups(self.classes)
+
+        # Choose a race
         self.race = race
         if not self.race:
-            self.race = get_random_race_by_class(self.char_class)
+            self.race = get_random_race_by_classes(self.classes)
+
+        # Assign alignment
         self.alignment = alignment
         if not self.alignment:
-            self.alignment = random.choice(CLASSES[self.char_class]["Alignments"])
+            self.alignment = get_alignment_by_classes(self.classes)
+
+        # Calculate experience
         if experience is not None:
             self.experience = experience
-            self.level = get_level_by_experience(self.char_class, experience)
+            self.levels = get_levels_by_experience(self.classes, experience)
         else:
-            self.level = level
-            self.experience = get_random_experience_by_level(
-                self.char_class, self.level
-            )
+            self.levels = [level for x in self.classes]
+            self.experience = get_random_experience_by_levels(self.classes, self.levels)
+
+        # Roll abilities if necessary
         self.abilities = abilities
         if not self.abilities:
             minimums = combine_minimums(
-                [CLASSES[self.char_class]["Minimums"], RACES[self.race]["Minimums"]]
+                [CLASSES[x]["Minimums"] for x in classes]
+                + [RACES[self.race]["Minimums"]]
             )
             ability_rolls = ABILITY_ROLLS[str(self.level)]
             self.abilities = get_abilities(
-                get_ability_priority(self.char_class),
+                get_ability_priority(self.classes),
                 minimums,
                 RACES[self.race]["Maximums"],
                 RACES[self.race]["Ability Modifiers"],
                 order=ability_rolls,
-                extrao_str=self.class_group == "Warrior" and self.race != "Halfling",
+                extrao_str="Warrior" in self.class_groups and self.race != "Halfling",
             )
-        self.level_limit = RACES[self.race]["Limits"][
-            self.char_class
-        ] + level_limit_bonus(self.char_class, self.abilities)
-        if self.level > self.level_limit:
-            self.level = self.level_limit
-        self.hitpoints = roll(
-            CLASS_GROUPS[self.class_group]["Hit Dice"][self.level - 1][0],
-            CLASS_GROUPS[self.class_group]["Hit Die"],
-            CLASS_GROUPS[self.class_group]["Hit Dice"][self.level - 1][1]
-            + self.level
-            * constitution_hp_modifier(
-                self.abilities["Constitution"], self.class_group
-            ),
-        )
-        self.spell_levels = []
+
+        # Apply level limits
+        self.level_limits = get_level_limits(self.race, self.classes, self.abilities)
+        for index in range(0, len(self.levels)):
+            if self.levels[index] > self.level_limit[index]:
+                self.levels[index] = self.level_limit[index]
+
+        # Calculate hitpoints
+        # FIXME: The Con mod is being applied per level, not per HD
+        hps = []
+        for index in range(0, len(self.levels)):
+            class_group = self.class_groups[index]
+            level = self.levels[index]
+            hps.append(
+                roll(
+                    CLASS_GROUPS[class_group]["Hit Dice"][level - 1][0],
+                    CLASS_GROUPS[class_group]["Hit Die"],
+                    CLASS_GROUPS[class_group]["Hit Dice"][level - 1][1]
+                    + level
+                    * constitution_hp_modifier(
+                        self.abilities["Constitution"], self.class_groups
+                    ),
+                )
+            )
+        self.hp = int(sum(hps) / len(self.levels))
+
+        # Determine if this character can cast spells, and assign them if so
+        self.spell_levels = {}
         self.spells = {}
-        self.caster_group = get_caster_group(self.char_class)
-        if self.caster_group:
-            self.spell_levels = get_spell_levels(
-                self.char_class, self.level, self.abilities["Wisdom"]
-            )
+        self.caster_groups = get_caster_groups(self.classes)
+        if self.caster_groups:
+            for index in range(0, len(self.classes)):
+                self.spell_levels[class_name] = get_spell_levels(
+                    self.classes[index], self.levels[index], self.abilities["Wisdom"]
+                )
             self.populate_spells()
-        self.thac0 = THAC0[self.class_group][self.level - 1]
-        self.equipment = []
-        self.ac = 10 + dexterity_ac_mod(self.abilities["Dexterity"])
-        self.nwp_slots = (
-            CLASS_GROUPS[self.class_group]["Proficiencies"]["Nonweapon"][0]
-            + int(
-                level / CLASS_GROUPS[self.class_group]["Proficiencies"]["Nonweapon"][1]
-            )
-            + intelligence_bonus_proficiencies(self.abilities["Intelligence"])
+
+        # Assign proficiencies
+        self.nwp_slots = get_nwp_slots(
+            self.class_groups, self.level, self.abilities["Intelligence"]
         )
         self.profs = {"NWP": [], "Weapon": [], "Languages": ["Common"]}
-        if self.char_class == "Thief":
+        if "Thief" in self.classes:
             self.profs["Languages"].append("Thieves' Cant")
-        elif self.char_class == "Druid":
+        elif "Druid" in self.classes:
             self.druid_lang_known = 0
-
         self.assign_nwps()
-        self.characteristics = generate_characteristics(self.race, self.level)
+
+        # Determine misc. stats
+        self.thac0 = THAC0[self.class_group][self.level - 1]
+        self.ac = 10 + dexterity_ac_mod(self.abilities["Dexterity"])
+        self.equipment = []
+        self.characteristics = generate_characteristics(self.race, max(self.levels))
 
     def __str__(self):
         self.update_ac()
@@ -408,13 +493,17 @@ class Character(object):
             self.abilities["Strength"], self.abilities["Extrao Strength"]
         )
         s = f"{'-'*10}\n"
+
         ### Race, Class, Alignment, HP, AC, THAC0
-        s += f"{self.race} {self.char_class} {self.level} - {self.alignment}\n"
+        classes_str = "/".join(self.classes)
+        levels_str = "/".join(self.levels)
+        s += f"{self.alignment} {self.race} {classes_str} {levels_str}"
         s += f"XP: {self.experience:,} - "
         if self.level_limit > 98:
             s += "Unlimited level limit\n"
         else:
-            s += f"Level limit: {self.level_limit}\n"
+            level_limits_str = "/".join(self.level_limits)
+            s += f"Level limit: {level_limits_str}\n"
         s += f"HP: {self.hitpoints}  AC: {self.ac}  THAC0: {self.thac0} (Melee:{self.thac0-str_hit_mod}/Ranged:{self.thac0-dex_hit_mod})\n"
 
         ### Abilities
@@ -462,7 +551,7 @@ class Character(object):
             ability = NWPS[nwp][1]
             if ability != "N/A":
                 modifier = self.abilities[ability] + NWPS[nwp][2]
-                if self.char_class == "Ranger" and nwp == "Tracking":
+                if "Ranger" in self.classes and nwp == "Tracking":
                     modifier += int(self.level / 3)
             s += f"\t{nwp:20} {ability:12}  Mod: {modifier:2}  Slots: {NWPS[nwp][0]}\n"
 
@@ -471,17 +560,18 @@ class Character(object):
 
         ### Spells
         if self.spell_levels:
-            s += "\n"
-            s += f"Spells ({'/'.join([str(x) for x in self.spell_levels])}):\n"
-            for spell_level in range(1, len(self.spell_levels) + 1):
-                s += f"\t{spell_level}: "
-                cur_spells = []
-                for spell in sorted(set(self.spells[spell_level])):
-                    count = ""
-                    if self.spells[spell_level].count(spell) > 1:
-                        count = f" ({self.spells[spell_level].count(spell)})"
-                    cur_spells.append(f"{spell}{count}")
-                s += f"{'; '.join(cur_spells)}\n"
+            s += "\nSpells:"
+            for class_name in self.spell_levels:
+                s += f"{class_name} ({'/'.join([str(x) for x in self.spell_levels[class_name]])}):\n"
+                for spell_level in range(1, len(self.spell_levels[class_name]) + 1):
+                    s += f"\t{spell_level}: "
+                    cur_spells = []
+                    for spell in sorted(set(self.spells[class_name][spell_level])):
+                        count = ""
+                        if self.spells[class_name][spell_level].count(spell) > 1:
+                            count = f" ({self.spells[class_name][spell_level].count(spell)})"
+                        cur_spells.append(f"{spell}{count}")
+                    s += f"{'; '.join(cur_spells)}\n"
 
         ### Equipment
         if self.equipment:
@@ -507,13 +597,14 @@ class Character(object):
     def assign_nwps(self):
         slots = self.nwp_slots
         possible_nwps = []
-        if self.char_class == "Bard":
+        if "Bard" in self.classes:
             self.profs["NWP"].append("Local History")
             self.profs["NWP"].append("Reading/Writing")
-        elif self.char_class == "Ranger":
+        elif "Ranger" in self.classes:
             self.profs["NWP"].append("Tracking")
-        for group in CLASSES[self.char_class]["Proficiency Groups"]:
-            possible_nwps += NWP_GROUPS[group]
+        for class_name in self.classes:
+            for group in CLASSES[class_name]["Proficiency Groups"]:
+                possible_nwps += NWP_GROUPS[group]
         possible_nwps = list(set(possible_nwps))
         while slots:
             # 50% chance for each slot to give demi-humans their racial language
@@ -541,10 +632,7 @@ class Character(object):
                     languages = RACES[self.race]["Languages"]
                 else:
                     languages = load_table("languages.json")
-                if (
-                    self.char_class == "Druid"
-                    and self.druid_lang_known / self.level > 1
-                ):
+                if "Druid" in self.classes and self.druid_lang_known / self.level > 1:
                     languages += load_table("druid_languages.json")
                 languages = list(set(languages) - (set(self.profs["Languages"])))
                 # Make sure this character hasn't learned all of their possible languages
@@ -560,50 +648,53 @@ class Character(object):
                 possible_nwps.remove(new_nwp)
 
     def populate_spells(self):
-        def add_spell(spell_level, spell):
-            if spell is None:
-                raise TypeError("Can't assign None as spell")
-            self.spells[spell_level].append(spell)
-
         spell_gen = Spells(expanded=self.expanded)
-        specialization = get_spell_specialization(self.char_class)
-        includes = get_spell_includes(self.char_class)
-        excludes = get_spell_excludes(self.char_class)
-        spell_subtype = get_spell_subtype(self.char_class)
-        for spell_level in range(1, len(self.spell_levels) + 1):
-            if spell_level not in self.spells:
-                self.spells[spell_level] = []
-            spell_count = self.spell_levels[spell_level - 1]
-            if specialization:
-                add_spell(
-                    spell_level,
-                    spell_gen.random_school_sphere_spell(
-                        spell_level, specialization, spell_subtype
-                    ),
-                )
-                spell_count -= 1
-            if excludes is not None:
-                for _ in range(0, spell_count):
+        for class_name in self.spell_levels:
+
+            def add_spell(spell_level, spell):
+                if spell is None:
+                    raise TypeError("Can't assign None as spell")
+                self.spells[class_name][spell_level].append(spell)
+
+            specialization = get_spell_specialization(class_name)
+            includes = get_spell_includes(class_name)
+            excludes = get_spell_excludes(class_name)
+            spell_subtype = get_spell_subtype(class_name)
+            for spell_level in range(1, len(self.spell_levels[class_name]) + 1):
+                if spell_level not in self.spells[class_name]:
+                    self.spells[class_name][spell_level] = []
+                spell_count = self.spell_levels[class_name][spell_level - 1]
+                if specialization:
                     add_spell(
                         spell_level,
-                        spell_gen.random_standard_school_sphere_spell(
-                            spell_level, spell_subtype, excludes=excludes
+                        spell_gen.random_school_sphere_spell(
+                            spell_level, specialization, spell_subtype
                         ),
                     )
-            elif includes is not None:
-                for _ in range(0, spell_count):
-                    add_spell(
-                        spell_level,
-                        spell_gen.random_include_school_sphere_spell(
-                            spell_level, spell_subtype, includes
-                        ),
-                    )
-            else:
-                caster_class = self.caster_group
-                for _ in range(0, spell_count):
-                    add_spell(
-                        spell_level, spell_gen.random_spell(spell_level, caster_class)
-                    )
+                    spell_count -= 1
+                if excludes is not None:
+                    for _ in range(0, spell_count):
+                        add_spell(
+                            spell_level,
+                            spell_gen.random_standard_school_sphere_spell(
+                                spell_level, spell_subtype, excludes=excludes
+                            ),
+                        )
+                elif includes is not None:
+                    for _ in range(0, spell_count):
+                        add_spell(
+                            spell_level,
+                            spell_gen.random_include_school_sphere_spell(
+                                spell_level, spell_subtype, includes
+                            ),
+                        )
+                else:
+                    caster_class = get_caster_group(class_name)
+                    for _ in range(0, spell_count):
+                        add_spell(
+                            spell_level,
+                            spell_gen.random_spell(spell_level, caster_class),
+                        )
 
     def update_ac(self):
         if "Bracers of Defenselessness" in self.equipment:
@@ -632,7 +723,7 @@ def main():
     )
     args = parser.parse_args()
     for class_name in get_all_classes():
-        print(Character(char_class=class_name, level=15, expanded=args.expanded))
+        print(Character(classes=[class_name], level=15, expanded=args.expanded))
 
 
 if __name__ == "__main__":
