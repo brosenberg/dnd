e9ab852393c7d5625fe0514e4c27de5759ea2bed
brosenberg <scdlbx@gmail.com>
2021-09-23 23:44:21 -0600

    Start to generate magic items in a cleaner way

--- 2e/items.py
+++ 2e/items.py
@@ -7,6 +7,7 @@ import os
 
 from dice import roll
 from magic_item import MagicItemGen
+from utils import do_action
 from utils import load_table
 
 
@@ -218,10 +219,71 @@ def random_shield():
 
 
 def random_special_magic_weapon():
-    special = random.choice(SPECIAL_MAGIC_WEAPON)
+    def compute_action(action, **kwargs):
+        action_type = SPECIAL_MAGIC_WEAPONS[special].get(f"{action} Type", None)
+        if action_type:
+            if action_type.startswith("Key_Roll"):
+                action_type = action_type.format(**kwargs)
+            return do_action(action_type, SPECIAL_MAGIC_WEAPONS[special]["Adjustment"])
+        return None
+
+    special = random.choice(list(SPECIAL_MAGIC_WEAPONS))
+
+    # Determine base item, if any
+    base_item = None
+    if not SPECIAL_MAGIC_WEAPONS[special].get("Generic Item", False):
+        try:
+            base_item = random.choice(SPECIAL_MAGIC_WEAPONS[special]["Base Items"])
+        except KeyError:
+            base_item = random.choice(
+                weapons_by_type(SPECIAL_MAGIC_WEAPONS[special]["Base Type"])
+            )
+
+    weapon = base_item
+    adjustment_str = ""
+    adjustment = compute_action("Adjustment", base_item=base_item)
+    charges_str = ""
+    charges_roll = SPECIAL_MAGIC_WEAPONS[special].get("Charges", None)
+    detail_str = ""
+    details = compute_action("Details", base_item=base_item)
+    detail_type = SPECIAL_MAGIC_WEAPONS[special].get("Detail Type", None)
+    quantity_roll = SPECIAL_MAGIC_WEAPONS[special].get("Quantity")
+
+    # Determine adjustment, if any
     try:
-        base_item = SPECIAL_MAGIC_WEAPON[special]
-    except KeyError:
+        if adjustment > 0:
+            adjustment_str = f" +{adjustment}"
+        elif adjustment < 0:
+            adjustment_str = f" -{adjustment}"
+    except TypeError:
+        pass
+
+    # Determine charges, if any
+    if charges_roll:
+        charges_str = (
+            f" ({roll(charges_roll[0], charges_roll[1], charges_roll[2])} charges)"
+        )
+
+    # Determine details, if applicable
+    if details:
+        detail_str = f" ({details})"
+
+    # Reformat name as specified
+    if not SPECIAL_MAGIC_WEAPONS[special].get("Ignore Name", False):
+        if SPECIAL_MAGIC_WEAPONS[special].get("Replace Name", False):
+            weapon = special
+        elif SPECIAL_MAGIC_WEAPONS[special].get("Prepend Name", False):
+            weapon = f"{special} {weapon}"
+        elif special.startswith(","):
+            weapon = f"{weapon}{special}"
+        else:
+            weapon = f"{weapon} {special}"
+    weapon = f"{weapon}{adjustment_str}{charges_str}{detail_str}"
+    if SPECIAL_MAGIC_WEAPONS[special].get("Cursed"):
+        weapon += " (Cursed)"
+    if quantity_roll:
+        weapon += f" x{roll(quantity_roll[0], quantity_roll[1], quantity_roll[2])}"
+    return weapon
 
 
 def random_weapon(
@@ -250,6 +312,10 @@ def random_weapon(
     return mig.diversify_weapon(random.choice(weapons))
 
 
+def weapons_by_type(weapon_type):
+    return [x for x in WEAPONS if WEAPONS[x]["Base Type"] == weapon_type]
+
+
 def main():
     # print(get_armor_ac("Hide armor +3"))
     # print(get_armor_ac("Leather of Blending +1"))
@@ -257,10 +323,12 @@ def main():
     # print(get_armor_ac("Plate Mail of Etherealness"))
     # for _ in range(0, 10):
     #    print(random_weapon())
-    print(appropriate_ammo_type("Light crossbow"))
-    print(appropriate_weapons_by_ammo("Flight arrow"))
-    print(base_weapon("Bastard sword +5"))
-    print(random_item_count("Dart"))
+    # print(appropriate_ammo_type("Light crossbow"))
+    # print(appropriate_weapons_by_ammo("Flight arrow"))
+    # print(base_weapon("Bastard sword +5"))
+    # print(random_item_count("Dart"))
+    for _ in range(0, 10):
+        print(random_special_magic_weapon())
 
 
 if __name__ == "__main__":
--- 2e/utils.py
+++ 2e/utils.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
 import json
+import random
 import os
+
+from dice import roll
+
+
+def do_action(action, options):
+    if action == "Choice":
+        return random.choice(options)
+    elif action.startswith("Key_Roll"):
+        key = action.split(" ")[1]
+        return roll(options[key][0], options[key][1], options[key][2])
+    elif action == "Roll":
+        return roll(options[0], options[1], options[2])
+    elif action == "Table":
+        table = {int(x): options[x] for x in options}
+        max_roll = max(table.keys())
+        result = roll(1, max_roll, 0)
+        for value in sorted(table):
+            if value >= result:
+                return table[value]
+        return table[max_roll]
 
 
 def intersect(list_a, list_b):
