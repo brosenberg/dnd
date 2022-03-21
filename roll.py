b7630953a415d44c824c2cd80555ef425b0b755b
brosenberg <scdlbx@gmail.com>
2022-03-21 11:15:53 -0600

    Improve code organization

--- roll.py
+++ roll.py
@@ -5,6 +5,17 @@ import re
 import sys
 
 
+def do_bonus(base, bonus_sign, bonus):
+    if bonus_sign == "+":
+        return base + bonus
+    if bonus_sign == "-":
+        return base - bonus
+    if bonus_sign == "/":
+        return base / bonus
+    if bonus_sign == "*":
+        return base * bonus
+
+
 def parse_dice(dice_str):
     rolls = []
     dice_re = r"([0-9]+d[0-9]+)([-+*/][0-9](?!d))?([-+*/])?"
@@ -43,19 +54,7 @@ def roll(dice, sides, bonus_sign, bonus):
     return result
 
 
-def do_bonus(base, bonus_sign, bonus):
-    if bonus_sign == "+":
-        return base + bonus
-    if bonus_sign == "-":
-        return base - bonus
-    if bonus_sign == "/":
-        return base / bonus
-    if bonus_sign == "*":
-        return base * bonus
-
-
-def main():
-    dices = parse_dice(re.sub(r"\s*", "", "".join(sys.argv[1:])))
+def roll_dices(dices, quiet=False):
     last_sign = ""
     result = 0
     for dice in dices:
@@ -65,13 +64,36 @@ def main():
         else:
             result = cur
         if dice[4]:
-            print(result)
+            if not quiet:
+                print(result)
             last_sign = dice[4]
-            print(last_sign)
+            if not quiet:
+                print(f"{last_sign} ", end="")
     if len(dices) == 1:
         return
-    print("=")
-    print(result)
+    if not quiet:
+        print("=")
+        print(result)
+    return result
+
+
+def print_usage():
+    print(f"Usage: {sys.argv[0]} [dice_str] ([dice_str] ...)")
+    print(f"   Ex: {sys.argv[0]} 3d6")
+    print(f"   Ex: {sys.argv[0]} '1d4+2+2d8-3d6+1*1d5/1d20'")
+
+
+def main():
+    dice_str = re.sub(r"\s*", "", "".join(sys.argv[1:]))
+    dices = parse_dice(dice_str)
+    if not dice_str:
+        print_usage()
+        sys.exit(1)
+    if not dices:
+        print(f"Unknown dice string: {dice_str}")
+        print_usage()
+        sys.exit(2)
+    roll_dices(dices)
 
 
 if __name__ == "__main__":
